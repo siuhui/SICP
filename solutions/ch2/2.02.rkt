81d17bd90776570e2b12a37ae612b6c1495f3f4d
siuhui <anrun388@gmail.com>
2024-02-28 20:57:46 +0800

    midpoint-segment

---
+++ solutions/ch2/2.02.rkt
@@ -0,0 +1,43 @@
+#lang sicp
+
+(define (make-segment s e)
+  (cons s e))
+
+(define (start-segment x)
+  (car x))
+
+(define (end-segment x)
+  (cdr x))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (midpoint-segment s)
+  (let ((x (average (x-point (start-segment s))
+                    (x-point (end-segment s))))
+        (y (average (y-point (start-segment s))
+                    (y-point (end-segment s)))))
+    (cons x y)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+;test
+(define one-two (make-point 1 2))
+(define three-six (make-point 3 6))
+(define segment (make-segment one-two three-six))
+(print-point (midpoint-segment segment))
