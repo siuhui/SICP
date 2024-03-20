462521d61b41da6b45d3453d60a2a187a3c504bf
siuhui <anrun388@gmail.com>
2024-03-20 23:35:00 +0800

    make-center-percent

---
+++ solutions/ch2/2.12.rkt
@@ -0,0 +1,22 @@
+#lang sicp
+
+(define (make-interval a b)
+  (cons a b))
+
+(define (upper-bound i)
+  (cdr i))
+
+(define (lower-bound i)
+  (car i))
+
+(define (make-center-percent c p)
+  (make-interval (- c (* c 0.01 p))
+                 (+ c (* c 0.01 p))))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i))
+     2))
+
+(define (percent i)
+  (/ (- (upper-bound i) (lower-bound i))
+     (* 2 (center i) 0.01)))
