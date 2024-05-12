94a9f819874706ef31b78db4272a653d5b587215
siuhui <anrun388@gmail.com>
2024-05-12 15:35:00 +0800

    tree

---
+++ solutions/ch2/2.24.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+(define tree (list 1 (list 2 (list 3 4))))
+
+
+(count-leaves tree); => 4
+tree; => (1 (2 (3 4)))
