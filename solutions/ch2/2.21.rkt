ca91fdfd72ac57bffe35d83f14380fa69141cac2
siuhui <anrun388@gmail.com>
2024-05-12 14:30:14 +0800

    square-list(map)

---
+++ solutions/ch2/2.21.rkt
@@ -0,0 +1,18 @@
+#lang sicp
+
+(define (square x)
+  (* x x))
+
+
+(define (square-list1 items)
+  (if (null? items)
+      nil
+      (cons (square (car items))
+            (square-list1 (cdr items)))))
+
+(define (square-list2 items)
+  (map square items))
+
+
+(square-list1 (list 1 2 3 4))
+(square-list2 (list 1 2 3 4))
