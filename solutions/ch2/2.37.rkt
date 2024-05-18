5f41a7ed6126de1b41d1da5fcdcc30f295c89997
siuhui <anrun388@gmail.com>
2024-05-18 14:57:52 +0800

    matrix

---
+++ solutions/ch2/2.37.rkt
@@ -0,0 +1,38 @@
+#lang sicp
+
+(define (fringe items)
+  (cond ((null? items) nil)
+        ((not (pair? items)) (list items))
+        (else (append (fringe (car items))
+                      (fringe (cdr items))))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (fringe (map (lambda (x) (car x)) seqs)))
+            (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (x)
+         (dot-product x v))
+       m))
+
+(define (transpose mat)
+  (accumulate-n cons nil mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (x)
+           (map (lambda (y)
+                  (dot-product x y))
+                cols))
+         m)))
